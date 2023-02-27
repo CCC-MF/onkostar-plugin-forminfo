9ea04f69b2960aaa5aae1fe5639943d82c586b08
Paul-Christian Volkmer <volkmer_p@ukw.de>
2023-02-27 14:26:10 +0100

    Do not include Onkostar API in this test class

--- src/test/java/de/ukw/ccc/onkostar/forminfo/FormInfoServiceTest.java
+++ src/test/java/de/ukw/ccc/onkostar/forminfo/FormInfoServiceTest.java
@@ -1,20 +1,12 @@
 package de.ukw.ccc.onkostar.forminfo;
 
-import de.itc.onkostar.api.IOnkostarApi;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(MockitoExtension.class)
 public class FormInfoServiceTest {
-
-    @Mock
-    private IOnkostarApi onkostarApi;
 
     @Test
     void testShouldFlattenSingleEntry() {
